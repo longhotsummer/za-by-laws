01ae52a8ca8dff14c5e67656bfd3451a42cc05ff
Greg Kempe <greg@kempe.net>
2016-04-17 16:50:02 +0200

    Fixes to auto-deploy script.

--- bin/push.sh
+++ bin/push.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
+# add git auth
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/archiver_rsa # this key should have push access
 ssh-add .travis/archiver_rsa
-git commit -m "Updates from Indigo via TravisCI"
+
 git config user.email "<hello@openbylaws.org.za>"
 git config user.name "OpenByLaws (via TravisCI)"
+git commit -m "Updates from Indigo via TravisCI"
 git remote set-url origin git@github.com:longhotsummer/za-by-laws.git
 git push
